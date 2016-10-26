4de73acdac685820e73ba81a742acac453fb233d
Alexander Rivera <ariveralee@ENIAC.local>
2016-10-25 22:43:13 -0400

    updated syntaxes, removed while loop

--- src/raspdetect.py
+++ src/raspdetect.py
@@ -16,19 +16,18 @@ rawCapture = PiRGBArray(camera, size=(320, 240))
 #allow camera to initialize
 time.sleep(0.1)
 
-While (True):
-	for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True)
-		img = frame.array
-		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-		faces = face_cascade.detectMultiScale(gray, 1.5, 3)
-		for (x, y, w, h) in faces:
-			cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True)
+	img = frame.array
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	faces = face_cascade.detectMultiScale(gray, 1.5, 3)
+	for (x, y, w, h) in faces:
+		cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-		cv2.imshow("Camera Feed", img)
-		key = cv2.waitKey(1) & 0xFF
-		rawCapture.truncate(0)
+	cv2.imshow("Camera Feed", img)
+	key = cv2.waitKey(1) & 0xFF
+	rawCapture.truncate(0)
 
-		if key == ord("q"):
-			break
+	if key == ord("q"):
+		break
 
 cv2.destroyAllWindows()
