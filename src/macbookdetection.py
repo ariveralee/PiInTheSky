6a06d264b9dd64f5adbd843a5b66484cf243925f
Alexander Rivera <ariveralee@gmail.com>
2016-11-10 22:59:54 -0500

    added count for making sure we have seen someones face

--- src/macbookdetection.py
+++ src/macbookdetection.py
@@ -7,17 +7,25 @@ face_cascade = cv2.CascadeClassifier(
 cap = cv2.VideoCapture(0)
 cap.set(3, 320)  # resize the frame
 cap.set(4, 240)
+count = 0
 
 while (True):
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 2)  # looks for the face
+    if len(faces) != 0:
+        count += 1
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
     cv2.imshow('Camera Feed', img)
+
+    if count == 60:
+        count = 0
+        print('We have a human')
     if cv2.waitKey(1) & 0xFF == ord('q'):  # pressing q exits
         break
 
+print(count)
 cap.release()
 cv2.destroyAllWindows()
--- src/raspdetection.py
+++ src/raspdetection.py
@@ -8,13 +8,16 @@ import time
 face_cascade = cv2.CascadeClassifier(
     '/Users/ariveralee/opencv/data/lbpcascades/lbpcascade_frontalface.xml')
 
-#initialize camera
+# initialize camera
 camera = PiCamera()
 camera.resolution = (320, 240)
 camera.framerate = 30
 rawCapture = PiRGBArray(camera, size=(320, 240))
 
-#allow camera to initialize
+# keeps track of frames with face in it
+count = 0
+
+# allow camera to initialize
 time.sleep(0.1)
 
 # Takes in each frame from the camera and converts it to OpenCv's BGR format
@@ -22,15 +25,28 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	img = frame.array
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 	faces = face_cascade.detectMultiScale(gray, 1.5, 3)
+	
+	# checks to see if we have detected a face
+	if len(faces) != 0:
+		count++
+	
+	# Draws the rectangle around the face if it's detected
 	for (x, y, w, h) in faces:
 		cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
 
 
 	cv2.imshow("Camera Feed", img)		# Uses a GUI to display each frame
 	key = cv2.waitKey(1) & 0xFF			# Wait key to keep the video streaming
 	rawCapture.truncate(0)				# removes each frame after it's displayed
 
+	# Makes sure that we have enough frames of someones face.
+	if count == 30:
+		count = 0
+		print("Found a human")
+	
 	if key == ord("q"):					# Pressing q quits the program			
 		break
 
+print(count)
 cv2.destroyAllWindows() 
